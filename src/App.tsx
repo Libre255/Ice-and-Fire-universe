85ff3a4a4736e5611ce8d11b30d543ea4ff8a293
Libre255 <briangbv@outlook.com>
2021-10-22 23:19:16 +0200

    implemented logo and background as well responsive design but need to doubel check for errors

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,26 @@ import React, { useState } from "react";
 import { useHouses } from "./Houses/Hooks/useHouses";
 import SearchBar from "./SearchBar/Components/SearchBar";
 import { filterByHouseName } from "./SearchBar/Methods/filterByHouseName";
+import Logo from "./Logo/Components/Logo";
 
 const App: React.FC = () => {
   const [searchInput, setSearchInput] = useState<string>("");
   const { listOfHouses } = useHouses();
 
   return (
-    <div>
-      <SearchBar
-        searchState={[searchInput, setSearchInput]}
-        listOfHouses={listOfHouses}
-      />
-      {listOfHouses
-        .filter((house) => filterByHouseName(house.name, searchInput))
-        .map((house, index) => (
-          <ol key={index}>{house.name}</ol>
-        ))}
+    <div id="hero">
+      <div id="wrapper">
+        <Logo/>
+        {/* <SearchBar
+          searchState={[searchInput, setSearchInput]}
+          listOfHouses={listOfHouses}
+        /> */}
+        {listOfHouses
+          .filter((house) => filterByHouseName(house.name, searchInput))
+          .map((house, index) => (
+            <ol key={index}>{house.name}</ol>
+          ))}
+      </div>
     </div>
   );
 };
