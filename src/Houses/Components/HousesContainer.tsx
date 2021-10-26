6e16decefb7ec4352368856c01f02db1ac5570f7
Libre255 <briangbv@outlook.com>
2021-10-26 21:28:50 +0200

    feature/5-display-houses-of-each-region

--- src/Houses/Components/HousesContainer.tsx
+++ src/Houses/Components/HousesContainer.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { HouseType } from "../../Service/HouseType";
 import House from "./Houses";
-
+import SearchBar from "../../SearchBar/Components/SearchBar";
+import { filterByHouseName } from "../../SearchBar/Methods/filterByHouseName";
 interface Props {
   setCloseRegionBtn: React.Dispatch<React.SetStateAction<boolean>>;
   houses: HouseType[];
 }
 
 const HousesContainer: React.FC<Props> = ({ setCloseRegionBtn, houses }) => {
+  const [searchInput, setSearchInput] = useState<string>("");
   const myref = useRef<HTMLDivElement>(null);
   useEffect(() => {
     myref.current!.scrollIntoView();
   }, []);
+
   return (
     <div id="HousesContainer" ref={myref}>
       <div
@@ -19,11 +22,16 @@ const HousesContainer: React.FC<Props> = ({ setCloseRegionBtn, houses }) => {
         className="button"
         onClick={() => setCloseRegionBtn(true)}
       />
-
+      <SearchBar
+        listOfHouses={houses}
+        searchState={[searchInput, setSearchInput]}
+      />
       <div id="HousesFlex">
-        {houses.map((house, index) => (
-          <House key={index} HouseInfo={house} />
-        ))}
+        {houses
+          .filter((house) => filterByHouseName(house.name, searchInput))
+          .map((house, index) => (
+            <House key={index} HouseInfo={house} />
+          ))}
       </div>
     </div>
   );
